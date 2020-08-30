47edb5de0aa3fd50cfeb30c0926d06f1696efd37
JeffaloBob <40470736+JeffaloBob@users.noreply.github.com>
2020-08-30 13:56:40 +0200

    add

--- addons/quotes/userscript.js
+++ addons/quotes/userscript.js
@@ -21,27 +21,18 @@ export default async function ({ addon, global, console }) {
 
         var overlay = document.createElement('div')
 
-        overlay.style.position = 'absolute'
-        overlay.style.display = 'block'
-        overlay.style.zIndex = '300'
-        overlay.style.lineHeight = '1.4'
-        overlay.style.padding = '5px 5px'
-        overlay.style.borderRadius = '5px'
-
-        overlay.style.backgroundColor = 'rgba(11,10,7,0.8)'
-        overlay.style.color = 'white'
+        overlay.className = 'add-quote'
 
 
         overlay.style.top = y + 10 + 'px'
-        overlay.style.left = x - 10 + 'px'
+        overlay.style.left = x + 'px'
         overlay.id = 'overlay'
 
         overlay.innerText = 'add as quote'
-        overlay.style.cursor = 'pointer'
 
         overlay.addEventListener('click', e => {
             document.getElementById('id_body').scrollTo()
-            textFieldEdit.insert(document.getElementById('id_body'), `[quote=${username}]${selection}[/quote]`)
+            textFieldEdit.insert(document.getElementById('id_body'), `[quote=${username}]${selection}[/quote]\n`)
         })
 
         document.body.appendChild(overlay)
@@ -57,19 +48,22 @@ export default async function ({ addon, global, console }) {
 
     document.addEventListener("selectionchange", debounce(function (event) {
         removeQuoteButton()
-        if (document.getSelection().focusNode.parentElement.className == 'post_body_html') {
-            let selection = document.getSelection ? document.getSelection().toString() : document.selection.createRange().toString();
-            console.log(selection);
+        console.log(document.getSelection())
+        if (document.getSelection().anchorNode.parentElement.className == 'post_body_html') { //todo check if anchor node is the same as focus node
+            //let selection = document.getSelection ? document.getSelection().toString() : document.selection.createRange().toString();
+            //console.log(selection);
 
-            selection = getSelectionHTML()
+            //let selection = htmlToBBCode(getSelectionHTML()) //needs some work
+
+            let selection = getSelectionHTML()
 
             var username = 'a person'
 
             if (selection == '') {
                 //
             } else {
-                if (document.getSelection().focusNode.parentElement.parentElement.parentElement.parentElement.parentElement.children[1]) {
-                    username = document.getSelection().focusNode.parentElement.parentElement.parentElement.parentElement.parentElement.children[1].children[0].children[0].children[0].innerText
+                if (document.getSelection().anchorNode.parentElement.parentElement.parentElement.parentElement.parentElement.children[1]) {
+                    username = document.getSelection().anchorNode.parentElement.parentElement.parentElement.parentElement.parentElement.children[1].children[0].children[0].children[0].innerText
                 }
                 addQuoteButton(selection, username)
             }
@@ -82,7 +76,7 @@ export default async function ({ addon, global, console }) {
         y = e.pageY
     })
 
-    function getSelectionHTML  () {
+    function getSelectionHTML() {
         var userSelection;
         if (window.getSelection) {
             // W3C Ranges
@@ -107,5 +101,67 @@ export default async function ({ addon, global, console }) {
         } else {
             return '';
         }
-    };
+    }
+
+    function htmlToBBCode(html) {
+
+        html = html.replace(/<pre(.*?)>(.*?)<\/pre>/gmi, "[code]$2[/code]");
+      
+          html = html.replace(/<h[1-7](.*?)>(.*?)<\/h[1-7]>/, "\n[h]$2[/h]\n");
+      
+          //paragraph handling:
+          //- if a paragraph opens on the same line as another one closes, insert an extra blank line
+          //- opening tag becomes two line breaks
+          //- closing tags are just removed
+          // html += html.replace(/<\/p><p/<\/p>\n<p/gi;
+          // html += html.replace(/<p[^>]*>/\n\n/gi;
+          // html += html.replace(/<\/p>//gi;
+      
+          html = html.replace(/<br(.*?)>/gi, "\n");
+          html = html.replace(/<textarea(.*?)>(.*?)<\/textarea>/gmi, "\[code]$2\[\/code]");
+          html = html.replace(/<b>/gi, "[b]");
+          html = html.replace(/<i>/gi, "[i]");
+          html = html.replace(/<u>/gi, "[u]");
+          html = html.replace(/<\/b>/gi, "[/b]");
+          html = html.replace(/<\/i>/gi, "[/i]");
+          html = html.replace(/<\/u>/gi, "[/u]");
+          html = html.replace(/<em>/gi, "[b]");
+          html = html.replace(/<\/em>/gi, "[/b]");
+          html = html.replace(/<strong>/gi, "[b]");
+          html = html.replace(/<\/strong>/gi, "[/b]");
+          html = html.replace(/<cite>/gi, "[i]");
+          html = html.replace(/<\/cite>/gi, "[/i]");
+          html = html.replace(/<font color="(.*?)">(.*?)<\/font>/gmi, "[color=$1]$2[/color]");
+          html = html.replace(/<font color=(.*?)>(.*?)<\/font>/gmi, "[color=$1]$2[/color]");
+          html = html.replace(/<link(.*?)>/gi, "");
+          html = html.replace(/<li(.*?)>(.*?)<\/li>/gi, "[*]$2");
+          html = html.replace(/<ul(.*?)>/gi, "[list]");
+          html = html.replace(/<\/ul>/gi, "[/list]");
+          html = html.replace(/<div>/gi, "\n");
+          html = html.replace(/<\/div>/gi, "\n");
+          html = html.replace(/<td(.*?)>/gi, " ");
+          html = html.replace(/<tr(.*?)>/gi, "\n");
+      
+          html = html.replace(/<img(.*?)src="(.*?)"(.*?)>/gi, "[img]$2[/img]");
+          html = html.replace(/<a(.*?)href="(.*?)"(.*?)>(.*?)<\/a>/gi, "[url=$2]$4[/url]");
+      
+          html = html.replace(/<head>(.*?)<\/head>/gmi, "");
+          html = html.replace(/<object>(.*?)<\/object>/gmi, "");
+          html = html.replace(/<script(.*?)>(.*?)<\/script>/gmi, "");
+          html = html.replace(/<style(.*?)>(.*?)<\/style>/gmi, "");
+          html = html.replace(/<title>(.*?)<\/title>/gmi, "");
+          html = html.replace(/<!--(.*?)-->/gmi, "\n");
+      
+          html = html.replace(/\/\//gi, "/");
+          html = html.replace(/http:\//gi, "http://");
+      
+          html = html.replace(/<(?:[^>'"]*|(['"]).*?\1)*>/gmi, "");
+          html = html.replace(/\r\r/gi, ""); 
+          html = html.replace(/\[img]\//gi, "[img]");
+          html = html.replace(/\[url=\//gi, "[url=");
+      
+          html = html.replace(/(\S)\n/gi, "$1 ");
+      
+          return html;
+      }
 }
