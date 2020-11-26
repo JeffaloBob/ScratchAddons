d9589f32348becdbd56b7b1f6aa9d3b515624df1
jeffalo <jeffalo@users.noreply.github.com>
2020-11-26 17:08:43 +0000

    style: format code

--- addons/hide-toolbox/userscript.js
+++ addons/hide-toolbox/userscript.js
@@ -1,46 +1,46 @@
 export default async function ({ addon, global, console }) {
-    const vm = addon.tab.traps.onceValues.vm;
+  const vm = addon.tab.traps.onceValues.vm;
 
-    var hidden = false
+  var hidden = false;
 
-    var toolbox;
-    var scrollbar;
+  var toolbox;
+  var scrollbar;
 
-    window.addEventListener('keydown', e => {
-        if (e.ctrlKey && e.key == 'h') {
-            e.preventDefault()
-            if (hidden) {
-                show()
-            } else {
-                hide()
-            }
-        }
-    })
-
-    function show() {
-        console.log('show')
-        toolbox.style.display = 'block'
-        scrollbar.style.display = 'block'
-        hidden = false
+  window.addEventListener("keydown", (e) => {
+    if (e.ctrlKey && e.key == "h") {
+      e.preventDefault();
+      if (hidden) {
+        show();
+      } else {
+        hide();
+      }
     }
+  });
 
-    function hide() {
-        console.log('hide')
-        toolbox.style.display = 'none'
-        scrollbar.style.display = 'none'
-        hidden = true
-    }
+  function show() {
+    console.log("show");
+    toolbox.style.display = "block";
+    scrollbar.style.display = "block";
+    hidden = false;
+  }
 
-    while (true) {
-        hidden = false
+  function hide() {
+    console.log("hide");
+    toolbox.style.display = "none";
+    scrollbar.style.display = "none";
+    hidden = true;
+  }
 
-        toolbox = await addon.tab.waitForElement(".blocklyFlyout", { markAsSeen: true });
-        scrollbar = await addon.tab.waitForElement(".blocklyFlyoutScrollbar", { markAsSeen: true });
-        let menu = document.querySelector('.blocklyToolboxDiv')
+  while (true) {
+    hidden = false;
 
-        console.log(toolbox, scrollbar, menu)
+    toolbox = await addon.tab.waitForElement(".blocklyFlyout", { markAsSeen: true });
+    scrollbar = await addon.tab.waitForElement(".blocklyFlyoutScrollbar", { markAsSeen: true });
+    let menu = document.querySelector(".blocklyToolboxDiv");
 
-/*         menu.addEventListener('mouseenter', e => {
+    console.log(toolbox, scrollbar, menu);
+
+    /*         menu.addEventListener('mouseenter', e => {
             console.log(e)
             if(hidden) show()
         })
@@ -49,6 +49,5 @@ export default async function ({ addon, global, console }) {
             console.log(e)
             if (!hidden) hide()
         }) */
-
-    }
+  }
 }
